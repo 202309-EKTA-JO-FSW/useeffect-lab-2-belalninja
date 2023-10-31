379864bf787829e3f3783362bec15e51d950cf94
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-10-31 07:49:08 +0200

    Done

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,10 +1,20 @@
-export default function Form() {
+export default function Form({ setusername }) {
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setusername(e.target.user.value);
+      }}
+    >
       <h3>Search user Github</h3>
       <div className="mb-3">
         <label>User name</label>
-        <input type="text" className="form-control" placeholder="User Name" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="User Name"
+          id="user"
+        />
       </div>
     </form>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
-import Form from '../Components/Form/Form'
-import UserCard from '../Components/UserCard/UserCard'
-import { Inter } from 'next/font/google'
-
-
-const inter = Inter({ subsets: ['latin'] })
+import Form from "../Components/Form/Form";
+import UserCard from "../Components/UserCard/UserCard";
+import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [username, setusername] = useState("");
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((r) => r.json())
+      .then((d) => setData(d));
+  }, [username]);
   return (
     <div className="card">
-      <Form />
-      <UserCard />
+      <Form setusername={setusername} />
+      <UserCard data={data} />
     </div>
   );
 }
